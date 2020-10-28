6205dfefce719efa05e549a63cadd69eca4d1582
Aleksandr Kraiz <selfsurfer@gmail.com>
2020-10-28 21:47:27 +0700

    Change default bitrate

--- screen-recording/background/background.contentScript.js
+++ screen-recording/background/background.contentScript.js
@@ -1,189 +1,221 @@
 var port = chrome.runtime.connect();
-port.onMessage.addListener(function(message) {
-    message.messageFromContentScript1234 = true
-    window.postMessage(message, '*');
+port.onMessage.addListener(function (message) {
+  message.messageFromContentScript1234 = true;
+  window.postMessage(message, "*");
 });
 
-window.addEventListener('message', function(event) {
-    if (event.source != window || !event.data.messageFromContentScript1234) return;
-    port.postMessage(event.data);
+window.addEventListener("message", function (event) {
+  if (event.source != window || !event.data.messageFromContentScript1234)
+    return;
+  port.postMessage(event.data);
 });
 
 // ctrl+tab to stop the recording
 var isControlKeyPressed;
-window.addEventListener('keydown', function(e) {
+window.addEventListener(
+  "keydown",
+  function (e) {
     keyCode = e.which || e.keyCode || 0;
 
-    if(keyCode === 17) {
-        isControlKeyPressed = true;
+    if (keyCode === 17) {
+      isControlKeyPressed = true;
     }
-}, false);
+  },
+  false
+);
 
-window.addEventListener('keyup', function(e) {
+window.addEventListener(
+  "keyup",
+  function (e) {
     keyCode = e.which || e.keyCode || 0;
 
-    if(isControlKeyPressed && keyCode === 32) {
-        port.postMessage({
-            messageFromContentScript1234: true,
-            stopRecording: true,
-            dropdown: true
-        });
+    if (isControlKeyPressed && keyCode === 32) {
+      port.postMessage({
+        messageFromContentScript1234: true,
+        stopRecording: true,
+        dropdown: true,
+      });
 
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
     }
 
-    if(keyCode === 17) {
-        isControlKeyPressed = false;
+    if (keyCode === 17) {
+      isControlKeyPressed = false;
     }
-}, false);
+  },
+  false
+);
 
 function RecordRTC_Extension(config) {
-    config = config || {
-        enableTabCaptureAPI: false,
-        enableTabCaptureAPIAudioOnly: false,
+  config = config || {
+    enableTabCaptureAPI: false,
+    enableTabCaptureAPIAudioOnly: false,
+    enableScreen: true,
+    enableSpeakers: true,
+    enableCamera: false,
+    enableMicrophone: false,
+    fixVideoSeekingIssues: false,
+    bitsPerSecond: 100000,
+    height: 1080,
+    width: 1920,
+  };
+
+  var startCallback = function () {
+    //
+  };
+
+  var stopCallback = function () {
+    //
+  };
+
+  var supportedValues = [
+    "enableTabCaptureAPI",
+    "enableTabCaptureAPIAudioOnly",
+    "enableScreen",
+    "enableSpeakers",
+    "enableCamera",
+    "enableMicrophone",
+    "fixVideoSeekingIssues",
+    "bitsPerSecond",
+    "height",
+    "width",
+  ];
+
+  window.addEventListener("message", function (event) {
+    if (event.source != window || !event.data.messageFromContentScript1234)
+      return;
+
+    if (event.data.startedRecording === true) {
+      startCallback();
+    }
+
+    if (event.data.stoppedRecording === true) {
+      stopCallback(dataURItoBlob(event.data.file));
+    }
+  });
+
+  // via: https://stackoverflow.com/a/12300351/552182
+  function dataURItoBlob(dataURI) {
+    var byteString = atob(dataURI.split(",")[1]);
+    var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    var blob = new Blob([ab], { type: mimeString });
+    return blob;
+  }
+
+  function getSupoortedFormats() {
+    return [
+      {
+        enableScreen: true,
+      },
+      {
+        enableScreen: true,
+        enableMicrophone: true,
+      },
+      {
         enableScreen: true,
         enableSpeakers: true,
-        enableCamera: false,
-        enableMicrophone: false,
-        fixVideoSeekingIssues: false,
-        height: 1080,
-        width: 1920,
-    };
-
-    var startCallback = function() {
-        //
-    };
-
-    var stopCallback = function() {
-        //
-    };
-
-    var supportedValues = [
-        'enableTabCaptureAPI',
-        'enableTabCaptureAPIAudioOnly',
-        'enableScreen',
-        'enableSpeakers',
-        'enableCamera',
-        'enableMicrophone',
-        'fixVideoSeekingIssues',
-        'height',
-        'width',
+      },
+      {
+        enableScreen: true,
+        enableMicrophone: true,
+        enableSpeakers: true,
+      },
+      {
+        enableTabCaptureAPI: true,
+      },
+      {
+        enableTabCaptureAPI: true,
+        enableTabCaptureAPIAudioOnly: true,
+      },
+      {
+        enableScreen: true,
+        enableCamera: true,
+      },
+      {
+        enableMicrophone: true,
+        enableCamera: true,
+      },
+      {
+        enableMicrophone: true,
+        enableSpeakers: true,
+      },
+      {
+        enableMicrophone: true,
+      },
+      {
+        enableSpeakers: true,
+      },
     ];
+  }
 
-    window.addEventListener('message', function(event) {
-        if (event.source != window || !event.data.messageFromContentScript1234) return;
-        
-        if(event.data.startedRecording === true) {
-            startCallback();
+  return {
+    startRecording: function (options, callback) {
+      startCallback = callback || function () {};
+
+      if (options) {
+        if (typeof options != "object") {
+          callback(false, "First parameter must be an object.");
+          return;
         }
 
-        if(event.data.stoppedRecording === true) {
-            stopCallback(dataURItoBlob(event.data.file));
+        if (!Object.keys(options).length) {
+          callback(
+            false,
+            "First parameter must specify at least one recoding type."
+          );
+          return;
         }
-    });
 
-    // via: https://stackoverflow.com/a/12300351/552182
-    function dataURItoBlob(dataURI) {
-      var byteString = atob(dataURI.split(',')[1]);
-      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-      var ab = new ArrayBuffer(byteString.length);
-      var ia = new Uint8Array(ab);
-      for (var i = 0; i < byteString.length; i++) {
-          ia[i] = byteString.charCodeAt(i);
+        var mismatchedValues = [];
+        Object.keys(options).forEach(function (key) {
+          if (supportedValues.indexOf(key) == -1) {
+            mismatchedValues.push(key);
+          }
+        });
+
+        if (mismatchedValues.length) {
+          callback(
+            false,
+            "Unsupported parameters detected: " + mismatchedValues.join(", ")
+          );
+          return;
+        }
+
+        config = options; // override
       }
-      var blob = new Blob([ab], {type: mimeString});
-      return blob;
-    }
 
-    function getSupoortedFormats() {
-        return [{
-            enableScreen: true
-        }, {
-            enableScreen: true,
-            enableMicrophone: true
-        }, {
-            enableScreen: true,
-            enableSpeakers: true
-        }, {
-            enableScreen: true,
-            enableMicrophone: true,
-            enableSpeakers: true
-        }, {
-            enableTabCaptureAPI: true
-        }, {
-            enableTabCaptureAPI: true,
-            enableTabCaptureAPIAudioOnly: true
-        }, {
-            enableScreen: true,
-            enableCamera: true
-        }, {
-            enableMicrophone: true,
-            enableCamera: true
-        }, {
-            enableMicrophone: true,
-            enableSpeakers: true
-        }, {
-            enableMicrophone: true
-        }, {
-            enableSpeakers: true
-        }];
-    }
+      config.messageFromContentScript1234 = true;
+      config.startRecording = true;
+      config.RecordRTC_Extension = true; // hint for background page
 
-    return {
-        startRecording: function(options, callback) {
-            startCallback = callback || function() {};
+      window.postMessage(config, "*");
+    },
+    stopRecording: function (callback) {
+      stopCallback = callback || function () {};
 
-            if(options) {
-                if(typeof options != 'object') {
-                    callback(false, 'First parameter must be an object.')
-                    return;
-                }
+      if (!config.startRecording) {
+        callback(false, "There is no recorder to stop.");
+        return;
+      }
 
-                if(!Object.keys(options).length) {
-                    callback(false, 'First parameter must specify at least one recoding type.')
-                    return;
-                }
+      config.startRecording = false;
+      config.stopRecording = true;
 
-                var mismatchedValues = [];
-                Object.keys(options).forEach(function(key) {
-                    if(supportedValues.indexOf(key) == -1) {
-                        mismatchedValues.push(key);
-                    }
-                });
-
-                if(mismatchedValues.length) {
-                    callback(false, 'Unsupported parameters detected: ' + mismatchedValues.join(', '))
-                    return;
-                }
-
-                config = options; // override
-            }
-
-            config.messageFromContentScript1234 = true;
-            config.startRecording = true;
-            config.RecordRTC_Extension = true; // hint for background page
-
-            window.postMessage(config, '*');
-        },
-        stopRecording: function(callback) {
-            stopCallback = callback || function() {};
-            
-            if(!config.startRecording) {
-                callback(false, 'There is no recorder to stop.');
-                return;
-            }
-
-            config.startRecording = false;
-            config.stopRecording = true;
-
-            window.postMessage(config, '*');
-        },
-        getSupoortedFormats: getSupoortedFormats
-    }
+      window.postMessage(config, "*");
+    },
+    getSupoortedFormats: getSupoortedFormats,
+  };
 }
 
-var script = document.createElement('script');
-(document.head || document.body || document.documentElement).appendChild(script).text = RecordRTC_Extension.toString() + ';;';
+var script = document.createElement("script");
+(document.head || document.body || document.documentElement).appendChild(
+  script
+).text = RecordRTC_Extension.toString() + ";;";
 script.remove();
--- screen-recording/background/background.messaging.js
+++ screen-recording/background/background.messaging.js
@@ -1,92 +1,100 @@
 var runtimePort;
 
-chrome.runtime.onConnect.addListener(function(port) {
-    runtimePort = port;
+chrome.runtime.onConnect.addListener(function (port) {
+  runtimePort = port;
 
-    runtimePort.onMessage.addListener(function(message) {
-        if (!message || !message.messageFromContentScript1234) {
-            return;
-        }
+  runtimePort.onMessage.addListener(function (message) {
+    if (!message || !message.messageFromContentScript1234) {
+      return;
+    }
 
-        if (message.startRecording) {
-            if(message.onlyMicrophone && enableCamera) {
-                message.startRecording = false;
-                message.stopRecording = true;
-                alert('Unable to access camera device.');
-                setDefaults();
-                return;
-            }
-        }
+    if (message.startRecording) {
+      if (message.onlyMicrophone && enableCamera) {
+        message.startRecording = false;
+        message.stopRecording = true;
+        alert("Unable to access camera device.");
+        setDefaults();
+        return;
+      }
+    }
 
-        if (message.startRecording) {
-            if(message.dropdown) {
-                openPreviewOnStopRecording = true;
-                openCameraPreviewDuringRecording = true;
-            }
+    if (message.startRecording) {
+      if (message.dropdown) {
+        openPreviewOnStopRecording = true;
+        openCameraPreviewDuringRecording = true;
+      }
 
-            if (isRecording && message.dropdown) {
-                stopScreenRecording();
-                return;
-            }
+      if (isRecording && message.dropdown) {
+        stopScreenRecording();
+        return;
+      }
 
-            if(message.RecordRTC_Extension) {
-                openPreviewOnStopRecording = false;
-                openCameraPreviewDuringRecording = false;
-                
-                enableTabCaptureAPI = message['enableTabCaptureAPI'] === true;
-                enableTabCaptureAPIAudioOnly = message['enableTabCaptureAPIAudioOnly'] === true;
-                enableScreen = message['enableScreen'] === true;
-                enableMicrophone = message['enableMicrophone'] === true;
-                enableCamera = message['enableCamera'] === true;
-                enableSpeakers = message['enableSpeakers'] === true;
-                fixVideoSeekingIssues = message['fixVideoSeekingIssues'] === true;
-                width = Number.isInteger(message['width']) ? message['width'] : 1920;
-                height = Number.isInteger(message['height']) ? message['height'] : 1080;
+      if (message.RecordRTC_Extension) {
+        openPreviewOnStopRecording = false;
+        openCameraPreviewDuringRecording = false;
 
-                startRecordingCallback = function(file) {
-                    port.postMessage({
-                        messageFromContentScript1234: true,
-                        startedRecording: true
-                    });
-                };
+        enableTabCaptureAPI = message["enableTabCaptureAPI"] === true;
+        enableTabCaptureAPIAudioOnly =
+          message["enableTabCaptureAPIAudioOnly"] === true;
+        enableScreen = message["enableScreen"] === true;
+        enableMicrophone = message["enableMicrophone"] === true;
+        enableCamera = message["enableCamera"] === true;
+        enableSpeakers = message["enableSpeakers"] === true;
+        fixVideoSeekingIssues = message["fixVideoSeekingIssues"] === true;
+        bitsPerSecond = message["bitsPerSecond"] === 100000;
+        width = Number.isInteger(message["width"]) ? message["width"] : 1920;
+        height = Number.isInteger(message["height"]) ? message["height"] : 1080;
 
-                chrome.storage.sync.set({
-                    enableTabCaptureAPI: enableTabCaptureAPI ? 'true' : 'false',
-                    enableTabCaptureAPIAudioOnly: enableTabCaptureAPIAudioOnly ? 'true' : 'false',
-                    enableMicrophone: enableMicrophone ? 'true' : 'false',
-                    enableCamera: enableCamera ? 'true' : 'false',
-                    enableScreen: enableScreen ? 'true' : 'false',
-                    enableSpeakers: enableSpeakers ? 'true' : 'false',
-                    fixVideoSeekingIssues: fixVideoSeekingIssues ? 'true' : 'false',
-                    videoResolutions: `${width}x${height}`,
-                    isRecording: 'true'
-                }, function() {
-                    getUserConfigs();
-                });
-                return;
-            }
+        startRecordingCallback = function (file) {
+          port.postMessage({
+            messageFromContentScript1234: true,
+            startedRecording: true,
+          });
+        };
 
+        chrome.storage.sync.set(
+          {
+            enableTabCaptureAPI: enableTabCaptureAPI ? "true" : "false",
+            enableTabCaptureAPIAudioOnly: enableTabCaptureAPIAudioOnly
+              ? "true"
+              : "false",
+            enableMicrophone: enableMicrophone ? "true" : "false",
+            enableCamera: enableCamera ? "true" : "false",
+            enableScreen: enableScreen ? "true" : "false",
+            enableSpeakers: enableSpeakers ? "true" : "false",
+            fixVideoSeekingIssues: fixVideoSeekingIssues ? "true" : "false",
+            bitsPerSecond: bitsPerSecond ? bitsPerSecond : 100000,
+            videoResolutions: `${width}x${height}`,
+            isRecording: "true",
+          },
+          function () {
             getUserConfigs();
-            return;
-        }
+          }
+        );
+        return;
+      }
 
-        if (message.stopRecording) {
-            if(message.RecordRTC_Extension) {
-                stopRecordingCallback = function(file) {
-                    var reader = new FileReader();
-                    reader.onload = function(e) {
-                        port.postMessage({
-                            messageFromContentScript1234: true,
-                            stoppedRecording: true,
-                            file: e.target.result
-                        });
-                    };
-                    reader.readAsDataURL(file);
-                };
-            }
+      getUserConfigs();
+      return;
+    }
 
-            stopScreenRecording();
-            return;
-        }
-    });
+    if (message.stopRecording) {
+      if (message.RecordRTC_Extension) {
+        stopRecordingCallback = function (file) {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+            port.postMessage({
+              messageFromContentScript1234: true,
+              stoppedRecording: true,
+              file: e.target.result,
+            });
+          };
+          reader.readAsDataURL(file);
+        };
+      }
+
+      stopScreenRecording();
+      return;
+    }
+  });
 });
